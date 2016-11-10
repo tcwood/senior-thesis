92504d66d9dd5ae94c29ed835da18792ea670b28
Oolkii <benjaminolki@gmail.com>
2016-11-10 15:57:33 -0800

    npm init and eslint airbnb init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+      "es6": true,
+      "node": true
+  },
+  "extends": "airbnb",
+  "parserOptions": {
+      "ecmaFeatures": {
+          "experimentalObjectRestSpread": true,
+          "jsx": true
+      },
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react"
+  ]
+};
