0787590880630973b478253e5472dc8b3ccc5454
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-11-10 17:45:17 -0800

    Add meta-test tests

---
+++ __tests__/Intro-test.js
@@ -0,0 +1,15 @@
+/* eslint-env jest, amd */
+// __tests__/Intro-test.js
+import 'react-native';
+import React from 'react';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import Intro from '../testComponents/Intro';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Intro />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
