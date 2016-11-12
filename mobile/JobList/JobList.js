1efc8a5a82b25f91903ebfee0e65b130c7f40576
Sam Henderson <hendersonsam456@gmail.com>
2016-11-12 15:51:24 -0800

    put searchbar into its own component called SearchBar as it will be reused on several views

--- mobile/JobList/JobList.js
+++ mobile/JobList/JobList.js
@@ -7,10 +7,7 @@ import {
   Image,
   TextInput
 } from 'react-native';
-import Search from 'react-native-search-bar';
-import {
-  FontAwesome,
-} from '@exponent/vector-icons';
+import SearchBar from './searchBar.js'
 
 
 // Get 'vh' for stylesheet. 1*vh represents 1% of the height of the viewport
@@ -21,25 +18,6 @@ const vw = width/100;
 
 // create stylesheet object
 const jobStyles = StyleSheet.create({
-  bluePattern: {
-    height: 10*vh
-  },
-  searchBar: {
-    backgroundColor: 'rgba(255,255,255,0.15)',
-    borderRadius: 8,
-    fontWeight: "100",
-    height: 4*vh,
-    marginRight: 10*vw,
-    marginLeft: 10*vw,
-    marginTop: 4*vh,
-    width: 80*vw,
-    color: 'white',
-    paddingLeft:3*vw
-  },
-  hideSearchIcon: {
-    color: '#ffffff'
-  }
-  ,
   jobTypeBar: {
     height: 7*vh,
     backgroundColor: 'white',
@@ -49,9 +27,6 @@ const jobStyles = StyleSheet.create({
     textAlign: 'center',
     color: 'black',
     fontSize: 20
-  },
-  searchIcon: {
-    paddingTop:0.4*vh
   }
 });
 
@@ -59,42 +34,15 @@ class JobList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchText: ''
     };
-
   }
-
-  hideSearchIcon(text) {
-    var context = this;
-    this.setState({searchText: text}, function() {
-    });
-  }
-
   render() {
     return(
       <View>
-
-        <Image
-          style={jobStyles.bluePattern}
-          source={require('../assets/blue-pattern-background.png')}
-        >
-        <TextInput 
-          style={jobStyles.searchBar}
-          placeholder='       Search for job'
-          placeholderTextColor='white'
-          onChangeText={(text) => this.hideSearchIcon(text)}
-          value={this.state.searchText}
-        >
-          {this.state.searchText.length > 0 ? null : <FontAwesome name={'search'} size={20} style={jobStyles.searchIcon} color={'#ffffff'}></FontAwesome>}
-        </TextInput>
-        </Image>
-
+        <SearchBar/>
         <View style={jobStyles.jobTypeBar}>
-          <Text style={jobStyles.jobTypeText}> State searchText: {this.state.searchText} </Text>
+          <Text style={jobStyles.jobTypeText}> State searchText: </Text>
         </View>
-
-
-
       </View>
     )
   }
