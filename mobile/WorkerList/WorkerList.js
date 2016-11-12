cd657c6b29f791cdb8b32d89e2e103cc5b66556c
Oolkii <benjaminolki@gmail.com>
2016-11-12 10:18:05 -0800

    remove search bar

--- mobile/WorkerList/WorkerList.js
+++ mobile/WorkerList/WorkerList.js
@@ -6,18 +6,16 @@ import {
 } from 'react-native';
 import RowList from './components/tradieList/TradieList';
 import styles from './workerListStyles';
-import SearchBar from 'react-native-search-bar';
 
 const rowList = [
   { name: 'Kanye West', location: 'San Bernardino', expertise: 'Plumba', reviews: '9' },
-  { name: 'Kanye West', location: 'San Bernardino', expertise: 'Plumba', reviews: '9' }
+  { name: 'Kanye West', location: 'San Bernardino', expertise: 'Plumba', reviews: '9' },
 ];
 
 
 const WorkerList = () => (
   <View>
     <Text style={styles.title}> WorkerList </Text>
-    <SearchBar    />
     <RowList
       setOfTradies={rowList}
     />
