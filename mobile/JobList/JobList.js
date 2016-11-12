54588879fde09516f156e7f29336cfa98dbe10bd
Sam Henderson <hendersonsam456@gmail.com>
2016-11-11 18:43:19 -0800

    added blue patterned background

--- mobile/JobList/JobList.js
+++ mobile/JobList/JobList.js
@@ -1,13 +1,52 @@
 import React from 'react';
-
+import fakeJobData from './fakeJobData.js';
 import {
   View,
   Text,
+  StyleSheet,
+  Image
 } from 'react-native';
+import Search from 'react-native-search-bar';
 
+/* Rectangle 3: */
+
+
+
+// Get 'vh' for stylesheet. 1*vh represents 1% of the height of the viewport
+const Dimensions = React.Dimensions || require('Dimensions')
+const {width, height} = Dimensions.get('window');
+const vh = height/100;
+
+
+// create stylesheet object
+const jobStyles = StyleSheet.create({
+  bluePattern: {
+    height: 10*vh
+  },
+  searchBar: {
+    'backgroundColor': '#ffffff',
+    'borderRadius': 8,
+
+  }
+});
+
+// create JobList component
 const JobList = () => (
   <View >
-    <Text> Job List </Text>
+    <View style={jobStyles.container}>
+      <Image
+        style={jobStyles.bluePattern}
+        source={require('../assets/blue-pattern-background.png')}
+      >
+        <Search
+            ref='searchBar'
+            style={jobStyles.searchBar}
+            placeholder='Search projects'
+            />
+      </Image>
+
+      <Text> {fakeJobData[0].title} </Text>
+    </View>
   </View>
 );
 
---
+++ mobile/JobList/fakeJobData.js
@@ -0,0 +1,25 @@
+const fakeJobData = [
+  {
+    title: 'Shower Remodel',
+    location: 'San Francisco',
+    pay: '$32 /hour',
+    ownerName: 'John Anderson',
+    ownerImage: 'http://www.solotradie.com/wp-content/uploads/2015/04/tradesman_laptop.jpg'
+  },
+  {
+    title: 'Bathroom Fitting',
+    location: 'Oakland',
+    pay: '$24 /hour',
+    ownerName: 'Steve Smith',
+    ownerImage: 'http://www.publicliability.co/wp-content/uploads/2012/07/public-liability-insurance-for-tradesmen.jpg'
+  },
+  {
+    title: 'Toilet Repair',
+    location: 'Mountain View',
+    pay: '$28 /hour',
+    ownerName: 'Bob Jones',
+    ownerImage: 'http://landscaping-kwikfynd.com.au/pictures/product-images/landscaping/landscape-tradesman/landscape-tradesman-14.jpg'
+  }
+];
+
+export default fakeJobData;
