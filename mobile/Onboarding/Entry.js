2a6bcce590091fc334a8c8da04812cc3150a835c
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-11-13 13:25:49 -0800

    Make Entry styles importable

--- mobile/Onboarding/Entry.js
+++ mobile/Onboarding/Entry.js
@@ -3,60 +3,87 @@ import React from 'react';
 import {
   View,
   Text,
+  TouchableOpacity,
   TextInput,
   Dimensions,
   StyleSheet,
 } from 'react-native';
 
-const { height, width } = Dimensions.get('window');
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderWidth: 1,
-    alignItems: 'center',
-  },
-
-  header: {
-    marginTop: height * 0.25,
-  },
-
-  input: {
-    borderWidth: 1,
-    marginLeft: width * 0.15,
-    marginBottom: width * 0.01,
-    marginTop: height * 0.01,
-    borderColor: 'gray',
-    height: 30,
-    width: width * 0.7,
-  },
-});
 
 class Entry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      user: '',
+      pass: '',
     };
+    this.signin = this.signin.bind(this);
+    this.signup = this.signup.bind(this);
+    const { height, width } = Dimensions.get('window');
+
+    this.styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        borderWidth: 1,
+        alignItems: 'center',
+      },
+
+      boot: {
+
+      },
+
+      input: {
+        borderWidth: 1,
+        marginBottom: width * 0.01,
+        marginTop: height * 0.01,
+        borderColor: 'gray',
+        height: 30,
+        width: width * 0.7,
+      },
+    });
+  }
+
+  signin() {
+    const username = this.state.user;
+    if (username !== '') {
+      this.props.grantAccess(username, 'blah');
+    }
+  }
+
+  signup() {
+    console.log('Signing up');
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          maxLength={16}
-          placeholder="Enter your username here"
-          onChangeText={text => this.setState({ text })}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your password here"
-          onChangeText={text => this.setState({ text })}
-        />
+      <View style={this.styles.container}>
+        <Text style={this.styles.boot}> Image goes here! </Text>
+        <View >
+          <TextInput
+            style={this.styles.input}
+            maxLength={16}
+            placeholder="Enter your username here"
+            onChangeText={text => this.setState({ user: text })}
+          />
+          <TextInput
+            style={this.styles.input}
+            placeholder="Enter your password here"
+            onChangeText={text => this.setState({ pass: text })}
+          />
+        </View>
+        <TouchableOpacity onPress={this.signin}>
+          <Text> Signin </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.signup}>
+          <Text> Signup </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+Entry.propTypes = {
+  grantAccess: React.PropTypes.func.isRequired,
+};
 
 export default Entry;
