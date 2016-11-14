8427cfed62efb130384f58540d9e16d8eb14ac33
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-11-14 10:49:52 -0800

    Add basic signup

--- mobile/Onboarding/SignUp.js
+++ mobile/Onboarding/SignUp.js
@@ -3,13 +3,37 @@ import React from 'react';
 import {
   View,
   Text,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
 
-const SignUp = () => (
-  <View>
-    <Text> SignUp </Text>
-  </View>
-);
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Sign up here </Text>
+        <TextInput
+          placeholder="Bob Johnson"
+        />
+        <TouchableOpacity onPress={this.signup}>
+          <Text> Next </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+SignUp.propTypes = {
+  grantAccess: React.PropTypes.func.isRequired,
+  username: React.PropTypes.string,
+};
 
 export default SignUp;
