e6f3c8e07e24688aa0568a59bf8f236deb960509
Oolkii <benjaminolki@gmail.com>
2016-11-14 20:49:32 -0800

    delete test db

--- db/dbServer.js
+++
@@ -1,9 +0,0 @@
-var sequelize = new Sequelize('puffyShirts', 'puffyShirts', 'puffyJackets', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-});
