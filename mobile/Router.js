fd2c5e1148d22f5a41c2459d9d7df9f7f16107b2
Oolkii <benjaminolki@gmail.com>
2016-11-16 19:41:47 -0800

    add routes for addjob, addjobdetails, and addjobwhatwherewhen

--- mobile/Router.js
+++ mobile/Router.js
@@ -8,6 +8,9 @@ import WorkerListScreen from './WorkerList/WorkerList';
 import Messages from './Messages/Messages';
 import NavigationBar from './NavigationBar';
 import JobProfileScreen from './JobProfile/Profile';
+import AddJob from './JobList/AddJob/AddJob';
+import AddJobWhatWhereWhen from './JobList/AddJob/AddJobWhatWhereWhen';
+import AddJobDetails from './JobList/AddJob/AddJobDetails';
 
 export default createRouter(() => ({
   profile: () => ProfileScreen,
@@ -16,5 +19,7 @@ export default createRouter(() => ({
   workerList: () => WorkerListScreen,
   messages: () => Messages,
   navigationBar: () => NavigationBar,
-  peerProfile: () => ProfileScreen,
+  addJob: () => AddJob,
+  addJobWhatWhereWhen: () => AddJobWhatWhereWhen,
+  addJobDetails: () => AddJobDetails,
 }));
