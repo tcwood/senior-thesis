078c70e23addbed193a6ce49fe2db1e330605156
Oolkii <benjaminolki@gmail.com>
2016-11-16 19:38:08 -0800

    import addjobbutton for creating new jobs, clean unnecessary imports addbutton() to return button with navigator augment searchbar with addbutton()

--- mobile/JobList/JobList.js
+++ mobile/JobList/JobList.js
@@ -12,6 +12,7 @@ import SearchBar from './searchBar.js';
 import JobTypeFilter from './jobTypeFilter.js'
 import MapListToggle from './mapListToggle.js'
 import JobTile from './JobTile.js'
+import AddJobButton from './AddJob/AddJobButton';
 
 
 import {
@@ -20,15 +21,12 @@ import {
 
 
 // Get 'vh' for stylesheet. 1*vh represents 1% of the height of the viewport
-const Dimensions = React.Dimensions || require('Dimensions')
+const Dimensions = React.Dimensions || require('Dimensions');
 const {width, height} = Dimensions.get('window');
 const vh = height/100;
 const vw = width/100;
 
 // create stylesheet object
-const jobList = StyleSheet.create({
-
-});
 
 class JobList extends React.Component {
   constructor(props) {
@@ -36,12 +34,17 @@ class JobList extends React.Component {
     this.state = {
     };
   }
+  AddButton() {
+    return () => (<AddJobButton navigator={this.props.navigator} />);
+  }
   render() {
     return(
       <View>
-        <SearchBar/>
-        <JobTypeFilter/>
-        <MapListToggle/>
+        <View style={{ width, height: 25 * 2.75 }}>
+          <SearchBar leftButton={this.AddButton()} />
+        </View>
+        <JobTypeFilter />
+        <MapListToggle />
         <ScrollView>
           {fakeJobData.map((job, i) => 
             <JobTile job={job} key={i} navigator={this.props.navigator}/> 
