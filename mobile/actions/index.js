001cbf01f0234a86d5b77c1b9a25e040bb5320ea
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-11-19 10:58:07 -0800

    Redux thunk function for profile upload

--- mobile/actions/index.js
+++ mobile/actions/index.js
@@ -5,4 +5,15 @@ export default class Actions {
       token,
     };
   }
+
+  static updateProfile(diff, upload) {
+    return (dispatch, getState) => {
+      dispatch({ type: 'UPDATE_PROFILE', diff });
+      if (upload) {
+        const profile = getState().profile;
+        console.log('[TODO]: upload the following profile to the server', profile);
+        // upload profile to the server.
+      }
+    };
+  }
 }
