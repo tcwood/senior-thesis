0ba5cff0c669d750c1b609f0c77c788ea40871c4
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-11-21 21:44:48 -0800

    Add actions for tradielist

--- mobile/actions/index.js
+++ mobile/actions/index.js
@@ -7,6 +7,39 @@ export default class Actions {
     };
   }
 
+  static loadJobList() {
+    return {
+      type: 'SEOMTHING',
+    };
+  }
+
+  static getToken() {
+    return {
+      type: 'SOMETHINGELSE',
+    };
+  }
+
+  static loadWorkerList() {
+    return (dispatch, getState) => {
+      axios.get('http://127.0.0.1:3000/user/')
+      .then((response) => {
+        console.log('got all users');
+        if (response.data.length > 0) {
+          console.log('wat');
+          dispatch({
+            type: 'UPDATE_WORKERLIST',
+            workers: response.data,
+            latest: response.data[0].updatedAt,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log('error loading worker list from db', error);
+      });
+    };
+  }
+
+  // Updates the profile on the store
   static updateProfile(diff, upload) {
     return (dispatch, getState) => {
       dispatch({ type: 'UPDATE_PROFILE', diff });
@@ -33,11 +66,12 @@ export default class Actions {
           });
         })
         .catch((error) => {
-          console.log('error posting new user in database', error);
+          console.log('error posting new user in db', error);
         });
       }
     };
   }
+
   static newJob(jobDetails) {
     return (dispatch, getState) => {
       const profile = getState().profile;
@@ -53,11 +87,31 @@ export default class Actions {
       };
       // upload profile to the server after completing onboarding questions
       axios.post('http://127.0.0.1:3000/job/', newJob)
-      .then((response) => {
-        dispatch({type: 'ADD_JOB', job: newJob});
+      .then(() => {
+        dispatch({ type: 'ADD_JOB', job: newJob });
       })
       .catch((error) => {
         console.log('error posting new user in database', error);
+      });
+    };
+  }
+
+  // Updates the workerlist on the store
+  static updateWorkerList() {
+    return (dispatch, getState) => {
+      const latest = getState().workerList.latest;
+      console.log('the latest', latest);
+      axios.get(`http://127.0.0.1:3000/${latest}`)
+      .then((response) => {
+        if (response.data.length > 0) {
+          dispatch({
+            type: 'UPDATE_WORKERLIST',
+            workers: response.data,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log('error updating workerlist', error);
       });
     };
   }
