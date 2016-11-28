e935abfe4db86c316725da9dea68b87d1ec1bce7
Travis Wood <travis_cw@yahoo.com>
2016-11-27 23:12:36 -0800

    Create messages input text box and send button

--- mobile/components/Messages.js
+++ mobile/components/Messages.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {
   Text,
   TextInput,
+  TouchableOpacity,
   View,
   StyleSheet,
   Dimensions,
 } from 'react-native';
+
+import {
+  FontAwesome,
+} from '@exponent/vector-icons';
 
 // Stack overflow has the below import to use socket.io w/ React-native, but
 // doesn't seem necessary for the time being
@@ -13,21 +18,29 @@ import {
 
 import io from 'socket.io-client/dist/socket.io';
 
-const { width } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    height: height - 64,
+    borderColor: 'blue',
+    borderWidth: 1,
+    justifyContent: 'space-between',
   },
-  messageBox: {
-    alignSelf: 'flex-end',
+  sendRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     height: 40,
     width,
-    fontSize: 16,
     borderColor: 'gray',
     borderWidth: 1,
-    paddingLeft: 10,
-    paddingRight: 10,
+  },
+  inputText: {
+    width: width * 0.7,
+    height: 30,
+    borderColor: 'black',
+    borderWidth: 1,
+    fontSize: 16,
   },
 });
 class Messages extends React.Component {
@@ -38,12 +51,13 @@ class Messages extends React.Component {
     };
     // Right now, just using local host for testing... switch to dynamic link
     // later
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.socket = io('http://localhost:3000', { transports: ['websocket'] });
     // can use socket throughout this module by referencing this.socket.io
   }
 
   handleSubmit() {
-    this.socket.emit('message', this.state.message);
+    // this.socket.emit('message', this.state.message);
     this.setState({
       message: '',
     });
@@ -53,13 +67,22 @@ class Messages extends React.Component {
     return (
       <View style={styles.container}>
         <Text> Hello World of Messaging! </Text>
-        <TextInput
-          style={styles.messageBox}
-          onChangeText={message => this.setState({
-            message,
-          })}
-          defaultValue={'test'}
-        />
+        <View style={styles.sendRow}>
+          <TextInput
+            style={styles.inputText}
+            onChangeText={(message) => {
+              this.setState({ message });
+            }}
+            value={this.state.message}
+          />
+          <TouchableOpacity onPress={this.handleSubmit}>
+            <FontAwesome
+              name={'paper-plane-o'}
+              size={25}
+              color={'#434343'}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
