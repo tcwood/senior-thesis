119102bdd59bcaf2ae296d2b3d6ab36caa836a6d
Oolkii <benjaminolki@gmail.com>
2016-12-01 12:57:45 -0800

    change tradiebanner to joblist format

--- mobile/components/TradieBanner.js
+++ mobile/components/TradieBanner.js
@@ -3,38 +3,36 @@ import {
   View,
   Text,
 } from 'react-native';
-import TradiesExpertiseBanner from './bannerIcons/TradiesExpertiseBanner';
-import TradiesLocationBanner from './bannerIcons/TradiesLocationBanner';
-import TradiesReviewsBanner from './bannerIcons/TradiesReviewsBanner';
+import {
+  FontAwesome,
+} from '@exponent/vector-icons';
 import styles from '../styles/WorkerList';
 
-const TradieBanner = ({ expertise, location, reviews, Icon, name }) =>
+const TradieBanner = ({ profession, location, rating }) =>
   (
-    <View style={styles.banner}>
-      <Text style={styles.name}>{name}</Text>
-      <View className="tripleBanner" style={styles.tripleBanner}>
-        <TradiesExpertiseBanner
-          Icon={Icon('wrench')}
-          expertise={expertise}
-        />
-        <TradiesLocationBanner
-          Icon={Icon('location-arrow')}
-          location={location}
-        />
-        <TradiesReviewsBanner
-          Icon={Icon('star-o')}
-          reviews={reviews}
-        />
+    <View style={styles.iconSection}>
+      <View style={styles.leftIcon}>
+        <FontAwesome name={'wrench'} size={15} style={styles.iconFormat} color={'gray'}>
+          <Text style={styles.iconText}> {profession}</Text>
+        </FontAwesome>
+      </View>
+      <View style={styles.middleIcon}>
+        <FontAwesome name={'location-arrow'} size={15} style={styles.iconFormat} color={'gray'}>
+          <Text style={styles.middleText}> {location}</Text>
+        </FontAwesome>
+      </View>
+      <View style={styles.rightIcon}>
+        <FontAwesome name={'star-o'} size={15} style={styles.iconFormat} color={'gray'}>
+          <Text style={styles.iconText}> {rating}</Text>
+        </FontAwesome>
       </View>
     </View>
   );
 
 TradieBanner.propTypes = {
-  expertise: React.PropTypes.string,
+  profession: React.PropTypes.string,
   location: React.PropTypes.string,
-  reviews: React.PropTypes.array,
-  Icon: React.PropTypes.func,
-  name: React.PropTypes.string,
+  rating: React.PropTypes.number,
 };
 
 export default TradieBanner;
